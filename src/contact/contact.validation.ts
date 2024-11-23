431cbb689867fc1d20ca40424821bd489b0d52bc
unknown <agungajipradana@gmail.com>
2024-11-24 00:36:01 +0700

    Update Contact API

--- src/contact/contact.validation.ts
+++ src/contact/contact.validation.ts
@@ -12,4 +12,23 @@ export class ContactValidation {
     email: z.string().min(1).max(100).email().optional(), // Validates `email` as an optional string with email format and a maximum length of 100 characters.
     phone: z.string().min(1).max(20).optional(), // Validates `phone` as an optional string with a maximum length of 20 characters.
   });
+
+  // Static constant `UPDATE` defines the schema for validating data when updating an existing contact.
+  // The schema includes an `id` field and allows optional updates to other fields.
+  static readonly UPDATE: ZodType = z.object({
+    // Validates `id` as a required positive number.
+    id: z.number().positive(),
+
+    // Validates `first_name` as a required string with a minimum length of 1 and a maximum of 100 characters.
+    first_name: z.string().min(1).max(100),
+
+    // Validates `last_name` as an optional string with a maximum length of 100 characters.
+    last_name: z.string().min(1).max(100).optional(),
+
+    // Validates `email` as an optional string that must be in email format and have a maximum length of 100 characters.
+    email: z.string().min(1).max(100).email().optional(),
+
+    // Validates `phone` as an optional string with a maximum length of 20 characters.
+    phone: z.string().min(1).max(20).optional(),
+  });
 }
--- src/model/contact.model.ts
+++ src/model/contact.model.ts
@@ -11,9 +11,25 @@ export class ContactResponse {
 // The `CreateContactRequest` class represents the structure for creating a new contact.
 // It is used to validate and process incoming data when creating a contact.
 export class CreateContactRequest {
-  id: number; // Unique identifier for the contact (can be generated or provided).
   first_name: string; // The first name of the contact (required field).
   last_name?: string; // (Optional) The last name of the contact.
   email?: string; // (Optional) The email address of the contact.
   phone?: string; // (Optional) The phone number of the contact.
+}
+
+export class UpdateContactRequest {
+  // The unique identifier for the contact to be updated.
+  id: number;
+
+  // The first name of the contact (required field).
+  first_name: string;
+
+  // The last name of the contact (optional field).
+  last_name?: string;
+
+  // The email address of the contact (optional field).
+  email?: string;
+
+  // The phone number of the contact (optional field).
+  phone?: string;
 }
